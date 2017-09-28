a8f740b8478c693ae8ece23f95594818697163a9
Claude Berube <claude.berube@aero.bombardier.com>
2017-09-28 17:20:26 -0400

    replace loader location

--- annotatorDemoCordovaApp/www/scripts/index.js
+++ annotatorDemoCordovaApp/www/scripts/index.js
@@ -19,9 +19,33 @@
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        // Load the main app module to start the app
+        requirejs.config({
+            baseUrl: 'libraries/batAnnotatorWidget',
+            paths: {
+                'jquery' : 'jquery.v2'
+            },
+            'shim': {
+                'html2canvas': {
+                    deps: ['jquery'],
+                    exports: 'html2canvas'
+                },
+                'zwibbler': {
+                    deps: ['jquery'],
+                    exports: 'Zwibbler'}
+                ,
+                'domtoimage': {
+                    deps: ['jquery'],
+                    exports: 'domtoimage'
+                },
+                'canvg': {
+                    deps: ['jquery'],
+                    exports:  'canvg'
+                },
+            }
+        });
+
+                // Load the main app module to start the app
         requirejs(['batAnnotatorWidget'], function(App){
-            alert('init');
             App.init();
         });
     }
